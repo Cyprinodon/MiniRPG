028c582a48417f1b10567210b890fe36c16a1955
grabette dimitri <cyprinodon@hotmail.fr>
2022-06-12 20:09:20 +0200

    Ajoute : Struct Input chargé de stocker une option de commande utilisateur

---
+++ MiniRPG - FP/Game/Structs/Input.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace MiniRPG.Game.Structs
+{
+    struct Input
+    {
+        public ConsoleKey Key { get; set; }
+        public string Label { get => Key.ToString().ToLower(); }
+        public string Description { get; set; }
+
+        public Input(ConsoleKey key, string description)
+        {
+            Key = key;
+            Description = description;
+        }
+
+        public override string ToString()
+        {
+            return Label + " -> " + Description;
+        }
+    }
+}
