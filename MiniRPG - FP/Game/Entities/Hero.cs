a85aa7cf0ed3fdf07b3f5b49d3a041d49bea294f
grabette dimitri <cyprinodon@hotmail.fr>
2022-06-13 14:28:28 +0200

    Ajoute : documentation XML aux classes 'Hero' et 'Monster'

--- MiniRPG - FP/Game/Entities/Hero.cs
+++ MiniRPG - FP/Game/Entities/Hero.cs
@@ -2,45 +2,117 @@
 
 namespace MiniRPG.Game.Entities
 {
+    /// <summary>
+    /// Une entité capable de monter en niveau, attaquer, fuir, boire des potions et se reposer.
+    /// </summary>
     class Hero
     {
-        public string Name { get; set; }
-        public string Title { get; set; }
-        public Gender Gender { get; set; }
-        protected int InnerLevel;
-        public int Level 
+        /// <value>
+        /// <see cref="Name"/> est une propriété de type <see langword="string"/> représentant le nom du héros.
+        /// <example>(ex: Valérie)</example>
+        /// </value>
+        public string Name { get; set; } = "";
+
+        /// <value>
+        /// <see cref="Title"/> est une propriété de type <see langword="string"/> représentant l'appellation courante du héros.
+        /// <example><i>(ex: vaillant prince)</i></example>
+        /// </value>
+        public string Title { get; set; } = "";
+
+        /// <value>
+        /// <see cref="Gender"/> est une propriété de type <see cref="Structs.Gender"/> représentant le genre du héros.
+        /// </value>
+        public Gender Gender { get; set; } = Data.GENDER_CHOICES[0];
+
+        /// <value>
+        /// <see cref="Level"/> est une propriété de type <see langword="int"/> représentant le niveau du héros. 
+        /// </value>
+        public int Level { get; set; } = 1;
+
+        /// <value>
+        /// <see cref="Hp"/> est une propriété de type <see langword="int"/> représentant les points de vie restant du héros.
+        /// </value>
+        public int Hp { get; set; } = 1;
+
+        /// <value>
+        /// <see cref="MaxHp"/> est une propriété de type <see langword="int"/> représentant les points de vie maximum 
+        /// du héros.
+        /// </value>
+        public int MaxHp { get; set; } = 1;
+
+        /// <value>
+        /// <see cref="Power"/> est une propriété de type <see langword="int"/> représentant la puissance d'attaque brute 
+        /// du héros.
+        /// </value>
+        public int Power { get; set; } = 0;
+
+        /// <value>
+        /// <see cref="Xp"/> est une propriété de type <see langword="int"/> représentant la quantité de points d'expérience 
+        /// accumulés par le héros.
+        /// </value>
+        public int Xp { get; set; } = 0;
+
+        /// <value>
+        /// <see cref="Gold"/> est une propriété de type <see langword="int"/> représentant la quantité d'argent 
+        /// accumulée par le héros.
+        /// </value>
+        public int Gold { get; set; } = 0;
+
+        /// <value>
+        /// <see cref="Potion"/> est une propriété de type <see langword="int"/> représentant la quantité de potions 
+        /// détenues par le héros.
+        /// </value>
+        public int Potions { get; set; } = 0;
+
+        /// <value>
+        /// <see cref="LevelThreshold"/> est un accesseur de type <see langword="int"/> permettant d'obtenir 
+        /// la quantité d'expérience que le héros doit accumuler à son niveau pour passer au niveau suivant.
+        /// </value>
+        public int LevelThreshold
         {
-            get => InnerLevel;
-            set 
+            get
             {
                 int levelIndex = Level - 1 <= 0 ? 0 : Level - 1;
-                InnerLevel = value;
-                LevelThreshold = Data.LEVEL_THRESHOLDS[levelIndex];
+                return Data.LEVEL_THRESHOLDS[levelIndex];
             }
         }
-        protected int InnerHp;
-        public int Hp
-        {
-            get => InnerHp;
-            set
-            {
-                InnerHp = value;
-                IsDead = Hp <= 0;
-            }
-        }
-        public int MaxHp { get; set; }
-        public int Power { get; set; }
-        public int Xp { get; set; }
-        public int Gold { get; set; }
-        public int Potions { get; set; }
-        public int LevelThreshold { get; protected set; }
+
+        /// <value>
+        /// <see cref="GaveUp"/> est une propriété de type <see langword="bool"/> permettant de savoir si 
+        /// le joueur a demandé à quitter la partie.
+        /// </value>
         public bool GaveUp { get; set; } = false;
+
+        /// <value>
+        /// <see cref="HasWon"/> est une propriété de type <see langword="bool"/> permettant de savoir si le joueur 
+        /// a gagné la partie.
+        /// </value>
         public bool HasWon { get; set; } = false;
-        public bool IsDead { get; protected set; } = false;
+
+        /// <value>
+        /// <see cref="IsDead"/> est un accesseur de type <see langword="bool"/> permettant de savoir si le héros est mort.
+        /// <para></para>
+        /// <remark>
+        /// <i>Cette variable prends la valeur <c>true</c> lorsque les points de vie du héros sont inférieur à <c>1</c>.</i>
+        /// </remark>
+        /// </value>
+        public bool IsDead { get => Hp <= 0; }
+
+        /// <value>
+        /// <see cref="HasFled"/> est une propriété de type <see langword="bool"/> permettant de savoir si le héros 
+        /// a réussi sa tentative de fuite.
+        /// </value>
         public bool HasFled { get; set; } = false;
 
+        /// <summary>
+        /// Constructeur par défaut de <see cref="Hero"/>.
+        /// </summary>
         public Hero(){ }
 
+        /// <summary>
+        /// Constructeur capable de construire une copie d'un objet <see cref="Hero"/> existant.
+        /// </summary>
+        /// <param name="state"><see cref="Hero"/> à copier.</param>
         public Hero(Hero state)
         {
             Name = state.Name;
--- MiniRPG - FP/Game/Entities/Monster.cs
+++ MiniRPG - FP/Game/Entities/Monster.cs
@@ -1,16 +1,68 @@
 ﻿namespace MiniRPG.Game.Entities
 {
+    /// <summary>
+    /// Une entité capable d'attaquer et de lâcher un butin à sa mort.
+    /// </summary>
     class Monster
     {
+        /// <value>
+        /// <see cref="Name"/> est une propriété de type <see langword="string"/> représentant le type de monstre.
+        /// <example><i>(ex: Gobelin)</i></example>
+        /// </value>
         public string Name { get; set; }
+
+        /// <value>
+        /// <see cref="MaxHp"/> est une propriété de type <see langword="int"/> représentant les points de vie maximum 
+        /// du monstre.
+        /// </value>
         public int MaxHp { get; set; }
+
+        /// <value>
+        /// <see cref="Hp"/> est une propriété de type <see langword="int"/> représentant les points de vie restant du monstre.
+        /// </value>
         public int Hp { get; set; }
+
+        /// <value>
+        /// <see cref="MaxHp"/> est une propriété de type <see langword="int"/> représentant les points de vie maximum 
+        /// du monstre.
+        /// </value>
+        /// 
+        /// <value>
+        /// <see cref="Power"/> est une propriété de type <see langword="int"/> représentant la puissance d'attaque brute 
+        /// du monstre.
+        /// </value>
         public int Power { get; set; }
+
+        /// <value>
+        /// <see cref="Gold"/> est une propriété de type <see langword="int"/> représentant la quantité d'argent 
+        /// lâché par le monstre à sa mort.
+        /// </value>
         public int Gold { get; set; }
+
+        /// <value>
+        /// <see cref="Xp"/> est une propriété de type <see langword="int"/> représentant la quantité de points d'expérience 
+        /// lâchés par le monstre à sa mort.
+        /// </value>
         public int Xp { get; set; }
 
-        public bool IsDead = false;
+        /// <value>
+        /// <see cref="IsDead"/> est un accesseur de type <see langword="bool"/> permettant de savoir si le monstre est mort.
+        /// <para>
+        /// <remark>
+        /// <i>Cette variable prends la valeur <c>true</c> lorsque les points de vie du monstre sont inférieur à <c>1</c>.</i>
+        /// </remark>
+        /// </para>
+        /// </value>
+        public bool IsDead { get => Hp <= 0; }
 
+        /// <summary>
+        /// Constructeur par défaut de <see cref="Monster"/> capable d'instancier un monstre à partir de son modèle de données.
+        /// <para>
+        /// Voir aussi:
+        /// <seealso cref="Structs.Monster"/>
+        /// </para>
+        /// </summary>
+        /// <param name="template">Le modèle de données du monstre.</param>
         public Monster(Structs.Monster template)
         {
             Name = template.Name;
@@ -18,6 +70,20 @@
             Power = template.Power;
             Gold = template.Gold;
             Xp = template.Xp;
+        }
+
+        /// <summary>
+        /// Constructeur de <see cref="Monster"/> capable d'instancier une copie d'un autre objet <see cref="Monster"/>.
+        /// </summary>
+        /// <param name="state">L'objet <see cref="Monster"/> à copier.</param>
+        public Monster(Monster state)
+        {
+            Name = state.Name;
+            MaxHp = state.MaxHp;
+            Hp = state.Hp;
+            Power = state.Power;
+            Gold = state.Gold;
+            Xp = state.Xp;
         }
     }
 }
